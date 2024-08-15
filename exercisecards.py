e62f8e3029bf14936876d0033a74196702997e2c
Matthew Trottier <116547304+rytrot@users.noreply.github.com>
2024-08-15 13:16:14 -0400

    Add files via upload

---
+++ exercisecards.py
@@ -0,0 +1,6 @@
+# make cards for musical exercises to practice in an anki deck
+#exercise 1: play chords (or chord progressions) in different intervals
+# 3 inversions: major, minor, diminished, augmented
+# 4 inversions: minor7, dominant7, major7, minor7b5, minor-major7, diminished7, major6, minor6, rootless ii, rootless V, rootless I
+# progressions: 2-5, 5-1, 2-5-1 (type A); 2-5, 5-1, 2-5-1 (type B); 2-5, 5-1, 2-5-1 (rootless type A); 2-5, 5-1, 2-5-1 (rootless type B); 2-5, 5-1, 2-5-1 (shell type A); 2-5, 5-1, 2-5-1 (shell type B)
+# intervals: semitone, tone, m3, M3, P4, P5 (up and down)
